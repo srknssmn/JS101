9445d4f2d4b648366fc13326b6fe316dc81555be
Serkan Sisman <srknssmn@gmail.com>
2022-09-10 18:44:21 +0300

    update

--- start.js
+++ start.js
@@ -1,1 +1,11 @@
-sds 
+// DEGISKEN TANIMLAMA
+//var serverName = "Go Go Go1";
+//console.log(serverName);
+
+//alert("MERHABA");   EKRANA UYARI MESAJI ÇIKARTMA!!
+
+let serverName, password;
+serverName = "Go Go Go";
+password = "1234"
+
+console.log(serverName+ " "+password);
