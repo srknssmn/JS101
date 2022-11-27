ef5e7a82eec88b561f64d3c34b84176733e10e51
Serkan Sisman <srknssmn@gmail.com>
2022-11-27 20:53:22 +0300

    edit

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -13,14 +13,41 @@ const startConf = () => {
 */
 
 let formDom = document.querySelector("#form")
+let taskDom = document.querySelector("#task")
 formDom.addEventListener("submit" , newElement )
 
-function newElement(event) {
-    event.preventDefault()
-    let taskDom = document.querySelector("#task")
+const startConf = () => {
+    const todos = JSON.parse(localStorage.getItem("todos"))     // Page ilk açılışında todos 'u localstorage'dan çekiyoruz.
+    
+    if(!todos) {
+        localStorage.setItem("todos" , JSON.stringify([]))  // Eğer sayfa ilk açılışında todos Arrey'i yoksa; Localstorage'a todos arreyini kayıt ediyoruz.
+    } else {
+        /*
+        const result = todos.filter(checkValue);
 
-    if (Boolean(taskDom.value.trim()) == true) {    // trim sağ ve soldaki boşlukları siliyor. Boolean ile value olup olmadığını kontrol ediyoruz.
-        liFunc(taskDom.value)
+        function checkValue(todoText) {
+            return (Boolean(todoText.trim()) == true)
+        }
+        */
+        todos.forEach(todoText => {                             // todos Array'ini gezerek Array içerisindeki tüm verileri fonksiyona atama yapıyor.
+        liFunc(todoText)
+        })
+    }
+}
+
+function newElement (event) {
+    event.preventDefault()
+    todoText = taskDom.value
+
+    const todos = JSON.parse(localStorage.getItem("todos"))    // todos 'u ARREY olarak localstorage'dan çekiyoruz.
+
+    if (Boolean(taskDom.value.trim()) == true) {
+        todos.push(todoText);
+    }                                     
+    localStorage.setItem("todos" , JSON.stringify(todos))       // Yeni veriyi tekrar localstorage'a kaydediyoruz.
+
+    if (Boolean(taskDom.value.trim()) == true) {                // trim sağ ve soldaki boşlukları siliyor. Boolean ile value olup olmadığını kontrol ediyoruz.
+        liFunc(todoText)                                        // todos Arrey'ine yeni veriyi ekliyoruz.
         $('.success').toast('show')        
     } else {
         $('.error').toast('show')
@@ -32,12 +59,12 @@ function liFunc (girdi) {
     let liDom = document.createElement("li")
     liDom.innerHTML = girdi
     listDom.append(liDom)
-
+// Li elementine check classı tanımlanıyor.
     liDom.addEventListener("click" , liCheck)
     function liCheck() {
         liDom.classList.toggle("checked")
     }
-
+// Li elementine X tuşu tanımlanıyor.
     let removeXBtn = document.createElement("span")
     let spanText = document.createTextNode("\u00D7")
     removeXBtn.appendChild(spanText)
@@ -50,6 +77,13 @@ function liFunc (girdi) {
     function removeFunc() {
         liDom.remove()
         $('.check').toast('show')
+
+        // BU BÖLÜME BAKILACAK!!
+        /*
+        const todos = JSON.parse(localStorage.getItem("todos"))
+        delete todos[3]
+        localStorage.setItem("todos" , JSON.stringify(todos))
+        */
     }
 }
 
