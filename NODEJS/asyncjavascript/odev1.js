2f99ae34db228ed549ea7f38fcc927088650ef7f
Serkan Sisman <srknssmn@gmail.com>
2022-12-15 01:30:08 +0300

    update

--- NODEJS/asyncjavascript/odev1.js
+++ NODEJS/asyncjavascript/odev1.js
@@ -39,4 +39,4 @@ async function addAndListPost(value) {
     }
 }
 
-addAndListPost({title: "Post4" , text: "lorem ipsum dolor" , sign: "Author4"});
+addAndListPost({title: "Post4" , text: "lorem ipsum dolor" , sign: "Author41"});
